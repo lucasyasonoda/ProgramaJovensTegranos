07d80262718f45bd6e5f729f825b4e69d6681834
lucasyasonoda <sonodaprog@gmail.com>
2022-11-30 01:34:23 -0300

    Input screen

---
+++ Exercício 1/index.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e) {
+    
+    e.preventDefault();
+
+    const widthInput  = document.querySelector("#width")
+    const lengthInput = document.querySelector("#length")
+    const valueMetter = document.querySelector("#valueM")
+
+    const width  = widthInput.value
+    const length = lengthInput.value
+    const valueM = valueMetter.value
+
+    
+    const area = areaTerreno(width, length)
+    const valor= precoTerreo(area, valueM)
+    alert(`Area do terreno = ${area.toFixed(2)}\nPreço do terreno = ${valor.toFixed(2)}`)
+    
+});
+
+function areaTerreno(width, length){
+    const area = width*length
+    return area
+}
+
+function precoTerreo(area, value){
+    const valueTer = area*value
+    return valueTer
+}   
+
+
+
+
