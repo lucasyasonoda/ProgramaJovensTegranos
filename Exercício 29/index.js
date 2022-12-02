079f249d108901d3a9e2bfe3818b76f60a9466d9
lucasyasonoda <sonodaprog@gmail.com>
2022-12-02 13:46:01 -0300

    not finished

--- Exercício 29/index.js
+++ Exercício 29/index.js
@@ -3,5 +3,7 @@ btn = document.querySelector("#send");
 btn.addEventListener("click", function(e) { 
     e.preventDefault();
 
+    const cnpjInput = document.querySelector("#cnpj");
 
+    let cnpj = cnpjInput.value;
 });
