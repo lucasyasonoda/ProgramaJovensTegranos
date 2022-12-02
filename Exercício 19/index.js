b8f9b858deaed9a2453edae5458f542987ecb744
lucasyasonoda <sonodaprog@gmail.com>
2022-12-02 13:18:14 -0300

    finished

--- Exercício 19/index.js
+++ Exercício 19/index.js
@@ -3,5 +3,29 @@ btn = document.querySelector("#send");
 btn.addEventListener("click", function(e) { 
     e.preventDefault();
 
+    const salarioInput = document.querySelector("#salario");
 
+    const salario = Number(salarioInput.value);
+
+
+    verificaSalario(salario);
 });
+
+function verificaSalario(s){  
+    aumentoPercent=0;
+    if(s <= 999.00){
+        aumentoPercent = 0.20;    
+    }else if(s >= 1000.00 && s <= 2999.00){
+        aumentoPercent = 0.15;
+    }else if(s >= 3000.00 && s <= 7999.00){
+        aumentoPercent = 0.10;
+    }else if(s >= 8000.00){
+        aumentoPercent = 0.05;
+    }
+    const novoSalario = s*(aumentoPercent+1);
+    const aumento = novoSalario-s;
+    console.log(`Novo salário = R$ ${novoSalario.toFixed(2)}`);
+    console.log(`Aumento = R$ ${aumento.toFixed(2)}`);
+    console.log(`Porcentagem = ${aumentoPercent*100} %`);
+
+}
