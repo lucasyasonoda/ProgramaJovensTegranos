f1359a7fb8341644386a64162026e52a57e7e6e5
lucasyasonoda <sonodaprog@gmail.com>
2022-12-03 00:25:13 -0300

    not yet

--- Exercício 29/index.js
+++ Exercício 29/index.js
@@ -5,5 +5,45 @@ btn.addEventListener("click", function(e) {
 
     const cnpjInput = document.querySelector("#cnpj");
 
-    let cnpj = cnpjInput.value;
+    let _cnpj = cnpjInput.value;
+    let cnpj  = _cnpj.split("-")[0].replace(/[.|\/]/g, "");
+    let dig   = _cnpj.split("-")[1]; 
+
+    validaCnpj(_cnpj);
+    primeiroVerDig(cnpj);
+    segundoVerDig(cnpj);
 });
+
+function primeiroVerDig(cnpj){
+    let peso = 2;
+    let resultado = 0;
+    for(let i=cnpj.length - 1;i > 0; i--){
+        resultado += cnpj[i] * peso;        
+        peso = (peso > 9 ? 2: peso++)
+    }
+    modulo = resultado % 11;
+    resultado = (modulo < 2 ? 0 : 11 - modulo);
+    return resultado;
+}
+
+function segundoVerDig(cnpj){
+    let peso = 2;
+    let resultado = 0;
+    for(let i=cnpj.length-1;i > 0; i--){
+        resultado += cnpj[i] * peso;        
+        peso = (peso > 9 ? 2: peso++)
+    }
+    modulo = resultado % 11;
+    resultado = (modulo < 2 ? 0 : 11 - modulo);
+    return resultado;
+}
+
+function validaCnpj(_cnpj, cnpj, dig){
+    if(primeiroVerDig(cnpj) != dig[0])
+        return "Saída: Inválido";
+    if(segundoVerDig(cnpj+""+dig[1]) != dig[0])
+        return "Saída: Inválido";
+    else
+        return "Saída: Válido";
+
+}
